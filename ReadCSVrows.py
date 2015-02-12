16caeacc4ebaa336f95fa2d8d01d81542a93f53c
meghanhuynh <meghanhuynh@users.noreply.github.com>
2015-02-12 14:15:10 -0500

    Changed delimiter condition
    "\n" to ","

--- ReadCSVrows.py
+++ ReadCSVrows.py
@@ -3,10 +3,11 @@ import csv
 # COUNT TOTAL ROWS/ENTRIES IN CSV FILE
 # NOTE: INCLUDES COLUMN HEADINGS ROW -> TOTAL ENTRIES = TOTALROWS - 1
 
-with open("/Users/huynhmeg/Desktop/custom-export/donorschoose-org-17feb2012-v1-essays.csv", "rt") as f:    
-    reader = csv.reader(f, delimiter="\n")
+with open("sample-essays.csv", "rt") as f:    
+    reader = csv.reader(f, delimiter=",")
 
     rows = list(reader)
     totalrows = len(rows)
+    totalentries = totalrows - 1
 
-    print (totalrows)
+    print ("Total Data Entries:", totalentries)
