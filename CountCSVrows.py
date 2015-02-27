3b30947a27a6c2c6d202d05e36875db435d6f317
meghanhuynh <meghanhuynh@users.noreply.github.com>
2015-02-27 13:16:22 -0500

    Update CountCSVrows.py

--- CountCSVrows.py
+++ CountCSVrows.py
@@ -5,7 +5,7 @@
 import csv
 import sys
 
-# DECREMENT LOOP FROM:
+# FOLLOWING CODE FROM:
 # http://stackoverflow.com/questions/15063936/csv-error-field-larger-than-field-limit-131072
 
 # Used to increase the field size 
@@ -27,20 +27,30 @@ while decrement:
 
 # COUNT TOTAL ROWS/ENTRIES IN CSV FILE
 # NOTES:
-#   1. DictReader automatically skips first row
+#   1. reader reads in first data entry
 #   2. Encoding parameter eliminates UNICODEDECODEERROR
+#   3. Rows include column heading -> Total Entries = Total Rows - 1
 
 with open("donorschoose-org-17feb2012-v1-essays.csv",
           'rt', encoding = 'latin1') as f:
     
-    reader = csv.DictReader(f, delimiter=",")
+    reader = csv.reader(f, delimiter=",")
 
-    totalrows=0
+    totalrows = 0
+    totalentries = 0
     
     for row in reader:
+        if totalrows== 1:
+            print (row)
         totalrows+=1
 
-    print ("Total Data Entries:", totalrows)
+    if totalrows == -1:
+        totalentries = 0
+        
+    else:
+        totalentries = totalrows - 1
+
+    print ("Total Data Entries:", totalentries)
 
 
 
